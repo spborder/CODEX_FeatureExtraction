ae9b5ab398c820ea0f358339365df1cdf14fa8c4
spborder <spborder@buffalo.edu>
2024-02-14 14:43:40 -0500

    Creating patch iterator

--- FeatureExtraction/feature_ext/cli/CODEX_FTX/CODEX_FTX.py
+++ FeatureExtraction/feature_ext/cli/CODEX_FTX/CODEX_FTX.py
@@ -20,12 +20,85 @@ Can annotations be appended to or just posted all at once?
 
 import os
 import sys
+from math import ceil, floor
+import numpy as np
 
 from ctk_cli import CLIArgumentParser
 
 sys.path.append('..')
 
 import girder_client
+
+
+class Patches:
+    def __init__(self,
+                 image_id:str,
+                 patch_size:int,
+                 region,
+                 gc):
+        
+        self.image_id = image_id
+        self.patch_size = patch_size
+        self.region = region
+        self.gc = gc
+
+        # Getting image metadata
+        self.image_info = self.gc.get(f'/item/{self.image_id}')
+        self.image_metadata = self.gc.get(f'/item/{self.image_id}/tiles')
+
+        if len(self.region) is None:
+            self.region = [
+                0,
+                0,
+                self.image_metadata['sizeX'],
+                self.image_metadata['sizeY']
+            ]
+
+        self.regions_list = self.get_regions_list()
+
+    def get_regions_list(self):
+        
+        # Defining list of all possible non-overlapping regions within the selected region
+
+        region_height = self.region[3]-self.region[1]
+        region_width = self.region[2]-self.region[0]
+
+        if region_height <= self.patch_size and region_width <= self.patch_size:
+            return [self.region]
+        else:
+            n_patch_x = ceil(region_width/self.patch_size)
+            n_patch_y = ceil(region_height/self.patch_size)
+
+            patch_regions_list = []
+
+            for x in range(0,n_patch_x):
+                for y in range(0,n_patch_y):
+
+                    # Finding patch start coordinates
+                    patch_start_x = np.minimum(self.region[0]+(x*self.patch_size),self.region[2]-(self.region[2]%self.patch_size))
+                    patch_start_y = np.minimum(self.region[0]+(y*self.patch_size),self.region[3]-(self.region[3]%self.patch_size))
+
+                    # Finding patch end coordinates
+                    patch_end_x = np.minimum(patch_start_x+self.patch_size, self.region[2])
+                    patch_end_y = np.minimum(patch_start_y+self.patch_size,self.region[3])
+
+                    patch_regions_list.append([patch_start_x,patch_start_y,patch_end_x,patch_end_y])
+            
+            return patch_regions_list
+
+    def __iter__(self):
+
+        self.patch_idx = -1
+
+        return self
+    
+    def __next__(self):
+
+        self.patch_idx+=1
+        if self.patch_idx<len(self.regions_list):
+            return self.regions_list[self.patch_idx]
+        else:
+            raise StopIteration
 
 
 def main(args):
@@ -57,8 +130,15 @@ def main(args):
     print(f'Image has {len(image_tiles_info["frames"])} Channels!')
     print(f'Image is {image_tiles_info["sizeY"]} x {image_tiles_info["sizeX"]}')
 
-    
+    # Creating patch iterator 
+    patch_maker = Patches(
+        image_id = image_id,
+        patch_size = args.patch_size,
+        region = args.input_region,
+        gc = gc
+    )
 
+    
 
 
 
